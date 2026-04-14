901716296c5b77423a96283083789d43bb7dab95
Nicolas Kusuma <nicolas.kusuma@magnusdigital.co.id>
2026-04-15 03:29:14 +0700

    Feat: Add New Feature - Supported Currencies

--- src/main/java/com/exchange/rate/constants/Constants.java
+++ src/main/java/com/exchange/rate/constants/Constants.java
@@ -3,4 +3,5 @@ package com.exchange.rate.constants;
 public class Constants {
 
 	public static final String HISTORICAL_IDR_USD = "historical_idr_usd";
+	public static final String SUPPORTED_CURRENCIES = "supported_currencies";
 }
